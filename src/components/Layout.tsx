d43cd392692cb0f7597cb7d562adc955d58e8cf9
sk97kang <sk97.kang@gmail.com>
2021-02-16 20:27:54 +0900

    feat: :sparkles: Add Layout
    Add Layout

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Head from "next/head";
+import Link from "next/link";
 import { APP_NAME } from "../common/constants";
 
 interface ILayoutProps {
@@ -10,6 +11,14 @@ export const Layout: React.FC<ILayoutProps> = ({
   children,
   title = APP_NAME,
 }) => {
+  const onLoginClick = () => {
+    console.log("Login");
+  };
+
+  const onCreateAccountClick = () => {
+    console.log("Create Account");
+  };
+
   return (
     <div>
       <Head>
@@ -17,7 +26,26 @@ export const Layout: React.FC<ILayoutProps> = ({
         <meta charSet="utf-8" />
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
       </Head>
-      {children}
+      <header className="bg-indigo-500 text-white">
+        <nav className="max-w-screen-md mx-auto flex justify-between items-center p-4">
+          <ul>
+            <li>
+              <Link href="/">
+                <a>Home</a>
+              </Link>
+            </li>
+          </ul>
+          <ul className="flex">
+            <li className="mr-4">
+              <button onClick={onLoginClick}>로그인</button>
+            </li>
+            <li>
+              <button onClick={onCreateAccountClick}>회원가입</button>
+            </li>
+          </ul>
+        </nav>
+      </header>
+      <main className="max-w-screen-md mx-auto">{children}</main>
     </div>
   );
 };
