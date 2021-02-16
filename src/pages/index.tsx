a5565b2a4c3058297e5bb354354b6226e0b1da9f
sk97kang <sk97.kang@gmail.com>
2021-02-16 20:13:19 +0900

    feat: :sparkles: Add tailwindcss
    Add tailwindcss

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,7 @@
 import { Layout } from "../components/Layout";
 
 const LendingPage = () => {
-  return (
-    <Layout>
-      <h1>Hello</h1>
-    </Layout>
-  );
+  return <Layout>Home</Layout>;
 };
 
 export default LendingPage;
