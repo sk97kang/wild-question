cc6b5a9ab623aec3317b9a005fa61e4767ea1961
sk97kang <sk97.kang@gmail.com>
2021-02-21 23:10:10 +0900

    Update

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -11,12 +11,25 @@ function MyApp({ Component, pageProps }: AppProps) {
         <title>{APP_NAME}</title>
         <meta charSet="utf-8" />
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <link rel="shortcut icon" href="/img/favicon.ico" sizes="64x64" />
+
+        <meta name="title" content="엉궁해" />
         <meta
           name="description"
-          content="엉뚱하지만 궁금해! 엉뚱한 질문을 올려보세요"
+          content="엉뚱하지만 궁금해! 엉뚱한 질문을 공유해보세요."
         />
-        <meta name="keywords" content="question,wild,질문,엉뚱" />
-        <link rel="shortcut icon" href="/img/favicon.ico" />
+        <meta name="keyword" content="Question, Comment, Wild, 질문, 엉뚱" />
+        <meta name="author" content="Kan" />
+
+        <meta property="og:title" content="엉궁해" />
+        <meta
+          property="og:description"
+          content="엉뚱하지만 궁금해! 엉뚱한 질문을 공유해보세요."
+        />
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://wild-question.netlify.app/" />
+        <meta property="og:image" content="/img/logo.png" />
+        <meta property="og:site_name" content="엉궁해" />
       </Head>
       <Component {...pageProps} />
     </RecoilRoot>
